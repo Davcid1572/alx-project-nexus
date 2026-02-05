65cdab08114cda87ad262d7fb5251419bd534d65
Davcid1572 <ifejiofordavid1572@gmail.com>
2026-02-05 10:11:49 +0100

    Refactor MainBanner component layout for improved structure and styling

--- e-commerce-product-catalog-app/components/MainBanner.tsx
+++ e-commerce-product-catalog-app/components/MainBanner.tsx
@@ -18,30 +18,31 @@ const MainBanner = () => {
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">
           Qualities You Can Trust, Savings You Will Love!
         </h1>
-      </div>
-      <div className="flex items-center mt-6 ml-3 font-medium ">
-        <Link
-          className="group z-100  flex items-center gap-2 px-7 md:px-9 py-3 bg-amber-400 hover:bg-amber-200 transition rounded text-white cursor-pointer"
-          href="/products"
-        >
-          Shop Now
-          <img
-            className="md:hidden transition group-focus:translate-x-1"
-            src={"/assets/images/white_arrow_icon.svg"}
-            alt="Shop Now Arrow"
-          />
-        </Link>
-        <Link
-          className="group z-100 hidden md:flex items-center gap-2 px-9 py-3"
-          href="/products"
-        >
-          Explore deals
-          <img
-            className=" transition group-focus:translate-x-1"
-            src={"/assets/images/black_arrow_icon.svg"}
-            alt="Shop Now Arrow"
-          />
-        </Link>
+
+        <div className="flex items-center mt-6  font-medium ">
+          <Link
+            className="group  flex items-center gap-2 px-7 md:px-9 py-3 bg-amber-400 hover:bg-amber-300 transition rounded text-white cursor-pointer"
+            href="/products"
+          >
+            Shop Now
+            <img
+              className="md:hidden transition group-focus:translate-x-1"
+              src={"/assets/images/white_arrow_icon.svg"}
+              alt="Shop Now Arrow"
+            />
+          </Link>
+          <Link
+            className="group hidden md:flex items-center gap-2 px-9 py-3"
+            href="/products"
+          >
+            Explore deals
+            <img
+              className=" transition group-focus:translate-x-1"
+              src={"/assets/images/black_arrow_icon.svg"}
+              alt="Shop Now Arrow"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
