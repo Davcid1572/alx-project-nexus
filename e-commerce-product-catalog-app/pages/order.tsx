66a79cd7a52af9e852eac81cbe36510c065c5d36
Davcid1572 <ifejiofordavid1572@gmail.com>
2026-02-07 13:43:21 +0100

    Created the order page to implement later

---
+++ e-commerce-product-catalog-app/pages/order.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const order = () => {
+  return (
+    <div className="flex items-center justify-center ">
+      Page under construction. This will be the order summary and checkout page.
+    </div>
+  );
+};
+
+export default order;
